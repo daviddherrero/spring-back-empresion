329c99584e296a24a1c66eeb64a2b82e64edc7c6
daviddherrero <dherrero3@gmail.com>
2022-02-06 19:44:08 +0100

    initil commit, project structure

---
+++ empresion/src/main/java/tfg/backend/empresion/empresion/EmpresionApplication.java
@@ -0,0 +1,13 @@
+package tfg.backend.empresion.empresion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresionApplication.class, args);
+	}
+
+}
---
+++ empresion/src/test/java/tfg/backend/empresion/empresion/EmpresionApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.backend.empresion.empresion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
